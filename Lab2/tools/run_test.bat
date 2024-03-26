c96bf51d21d95178baadc087ae2232bbabe50ce5
andrei-eduard-s <solomonandreieduard1@gmail.com>
2024-03-26 20:18:37 +0200

    Updated run_test.bat & .gitignore

--- Lab2/tools/run_test.bat
+++ Lab2/tools/run_test.bat
@@ -4,5 +4,8 @@ call clean.bat
 call build.bat
 ::========================================================================================
 cd ../sim
-:: vsim -gui -do run.do
-vsim -c -do run.do
+:: Daca comentam urmatoarea linie de cod, nu se va mai deschide interfata grafica:
+vsim -gui -do run.do
+
+::Daca rulam urmatoarea linie de cod, programul se va executa in consola:
+:: vsim -c -do run.do
