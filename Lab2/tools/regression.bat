05b859e93fcacc379ec6e3eb0e32edab4a0ccf19
andrei-eduard-s <solomonandreieduard1@gmail.com>
2024-04-17 08:37:39 +0300

    Laboratorul_9

--- Lab2/tools/regression.bat
+++ Lab2/tools/regression.bat
@@ -10,13 +10,15 @@
 @REM call run_test.bat 30 30 2 1 case_dec_rnd c
 @REM call run_test.bat 30 30 2 2 case_dec_dec c
 
-call run_test.bat 50 32 1 1 case_1 c 986435
-call run_test.bat 50 32 1 1 case_2 c 533164
-call run_test.bat 50 32 1 1 case_3 c 125753
-call run_test.bat 50 32 1 1 case_4 c 735753
+call run_test.bat 50 32 1 1 case_1 c 986025
+call run_test.bat 50 32 1 1 case_2 c 533114
+call run_test.bat 50 32 1 1 case_3 c 125553
+call run_test.bat 50 32 1 1 case_4 c 119953
 call run_test.bat 50 32 1 1 case_5 c 184750
-call run_test.bat 50 32 1 1 case_6 c 975321
+call run_test.bat 50 32 1 1 case_6 c 974321
 call run_test.bat 50 32 1 1 case_7 c 775331
 call run_test.bat 50 32 1 1 case_8 c 111008
 call run_test.bat 50 32 1 1 case_9 c 295716
 call run_test.bat 50 32 1 1 case_10 c 193755
+
+@REM run_test.bat 50 32 1 1 case_1 gui 986025
